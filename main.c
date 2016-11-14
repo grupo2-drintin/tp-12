abd327ad19bb2342852247357bbf836774da61ba
malustewart <mastewart@itba.edu.ar>
2016-11-13 21:20:19 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: malu
- *
- * Created on 13 de noviembre de 2016, 17:40
- */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include "events_kb.h"
+#include "nonblock.h"
 
 #define  ONE_MS    	1000	   // One mseg
 
@@ -26,43 +15,61 @@ int main()
     const char* excuse_pointers['z'-'a'+1] = {
 
     //excusas para no entregar un TP a tiempo
-    "Tuve que pasear a mi pez",
-    "Tengo falta de memoria a corto plazo y no se en que carpeta lo guarde",
-    "Mi cabra se comio mi computadora",
-    "Mi axolotl se comio mi computadora",
-    "Lo deje en mis otros zapatos",
-    "Intente pero stackoverflow estaba caido",
-    "Tarea? Yo? Sabes quien soy?",
-    "Bualo me formateo la compu",
-    "Lo hice pero lo borre porque no tenia espacio para bajarme la serie completa de My Little Pony",
-    "justo recien cai en la de sudo rm -rf/",
-    "Para que hacerlo si C ya es un lenguaje muerto?",
-    "Lo tengo en el drive, cuando ande el wifi del ITBA te lo mando",
-    "Nadie me dijo que si o si habia que conectar un pin de a rpi a tierra",
+    "Tuve que pasear a mi pez\n",
+    "Tengo falta de memoria a corto plazo y no se en que carpeta lo guarde\n",
+    "Mi cabra se comio mi computadora\n",
+    "Mi axolotl se comio mi computadora\n",
+    "Lo deje en mis otros zapatos\n",
+    "Intente pero stackoverflow estaba caido\n",
+    "Tarea? Yo? Sabes quien soy?\n",
+    "Bualo me formateo la compu\n",
+    "Lo hice pero lo borre porque no tenia espacio para bajarme la serie completa de My Little Pony\n",
+    "justo recien cai en la de sudo rm -rf/\n",
+    "Para que hacerlo si C ya es un lenguaje muerto?\n",
+    "Lo tengo en el drive, cuando ande el wifi del ITBA te lo mando\n",
+    "Nadie me dijo que si o si habia que conectar un pin de la rpi a tierra\n",
     
     //excusas para faltar al parcial
-    "Lamento no haber podido ir, pero no tenia ganas",
-    "No te puede creer! Juraba que era en dos dias",
-    "Me quede sin nafta para la bici",
-    "La alfombra voladora me dejo a pata"
+    "no tenia ganas\n",
+    "juraba que era en dos dias\n",
+    "me quede sin nafta para la bici\n",
+    "la alfombra voladora me dejo a pata\n",
+    "me dijieron que la H me dejaba en la puerta del ITBA\n",
+    "me regalaron un calcetin. Ya soy libre",
+    "se rompio el boton de planta baja de mi ascensor\n",
+    "se muerio mi pez\n",
+    "llovia y no queria arruinar mis guantes nuevos\n",
+    "me gano pachorra\n",
+    "primero un elfo domestico no me dejo entrar a la estacion, y despues un arbol malvado ataco mi auto volador\n",
+    "los Lannisters me mantuvieron cautivo todo el dia",
+    "estaba pronosticado el fin del mundo. Menos mal que sigo vivo para poder darlo la semana que viene"
     };
     
-    start_read_kb();
-    pthread_t printing_thread;
-    pthread_create(&printing_thread, NULL, print_typing, excuse_pointers[get_next_event()-'a']);
-    stop_read_kb();
-
-    return (0);
+    start_read_kb();   
+    pthread_t printing_thread;    
+    while (1)
+    {    
+        char i = get_next_event();
+        if (i>='a' && i<='q')
+        {
+            pthread_create(&printing_thread, NULL, print_typing, excuse_pointers[i-'a']);
+            pthread_join(printing_thread, NULL);
+        }
+    }
+    stop_kb_read();
+    return 0;
 }
 
-void * print_typing(void* start_of_string)  
+void* print_typing(void* start_of_string)  
 {
     int i = 0;    
     char key;                               //contiene caracter a imprimir
-    while(key = ((char*)start_of_string)[i])  //repetir hasta que se llegue al terminador
+    while(((char*)start_of_string)[i]!= 0)  //repetir hasta que se llegue al terminador
     {               
-        usleep(100*ONE_MS);                 // 100ms * 
-        putchar(key);
+        key = (((char*)start_of_string)[i]);
+        printf("%c",key);
+        sleep(0.01);
         i++;	
     }
 }
+ 
